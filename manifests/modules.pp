dbec04e5b432a7ec7cce5d55420d7adbfab97009
Eric Davis <edavis@littlestreamsoftware.com>
2009-08-21 14:33:04 -0700

    Adding submodules for Apache2 and Passenger

--- manifests/modules.pp
+++ manifests/modules.pp
@@ -2,3 +2,8 @@ import "puppet-ruby1.8"
 import "puppet-rubygems"
 import "puppet-mysql"
 import "puppet-mysql/database"
+
+import "puppet-apache2"
+import "puppet-apache2/site"
+import "puppet-passenger"
+import "puppet-passenger/enable"
